d5d49ec6c17b0848df90aea1ae2f45453b5b8002
cydream25 <sylvain.heck@telecomnancy.net>
2017-11-03 22:13:04 +0100

    add fixture Category

--- src/Cydream/PlatformBundle/Controller/AdvertController.php
+++ src/Cydream/PlatformBundle/Controller/AdvertController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Cydream\PlatformBundle\Entity\Advert;
 use Cydream\PlatformBundle\Entity\Image;
+use Cydream\PlatformBundle\Entity\Application;
 
 class AdvertController extends Controller
 {
@@ -96,6 +97,29 @@ class AdvertController extends Controller
 
     public function editAction($id, Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        
+        // On récupère l'annonce $id
+        $advert = $em->getRepository('OCPlatformBundle:Advert')->find($id);
+    
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+    
+        // La méthode findAll retourne toutes les catégories de la base de données
+        $listCategories = $em->getRepository('OCPlatformBundle:Category')->findAll();
+    
+        // On boucle sur les catégories pour les lier à l'annonce
+        foreach ($listCategories as $category) {
+            $advert->addCategory($category);
+        }
+    
+        // Pour persister le changement dans la relation, il faut persister l'entité propriétaire
+        // Ici, Advert est le propriétaire, donc inutile de la persister car on l'a récupérée depuis Doctrine
+    
+        // Étape 2 : On déclenche l'enregistrement
+        $em->flush();
+
         if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée');
             return $this->redirectToRoute('oc_platform_view', ['id' => 5]);
